8b0d436858255e152bf0e88bd51bfaf9f0b3d0b0
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-09-24 13:26:22 +0200

    update - ensure version is >= 3.2.0 to update to current

--- falocalrepo_database/update.py
+++ falocalrepo_database/update.py
@@ -402,7 +402,7 @@ def update_database(db: Connection) -> Connection:
         return update_database(update_3_to_3_1(db))
     elif v >= 0 and (v := compare_versions(db_version, "3.2.0")) < 0:
         return update_database(update_3_1_to_3_2(db))
-    elif v and compare_versions(db_version, __version__) < 0:
+    elif v >= 0 and compare_versions(db_version, __version__) < 0:
         return update_to_current(db)
 
     return db
