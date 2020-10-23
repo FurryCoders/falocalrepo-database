0296f557644553103465a1110abb24d6c8de0812
Matteo Campinoti <matteo.campinoti94@gmail.com>
2020-10-23 10:31:35 +0200

    merge - do not use fields and indexes values

--- falocalrepo_database/merge.py
+++ falocalrepo_database/merge.py
@@ -12,13 +12,10 @@ from typing import Tuple
 
 from .__version__ import __version__
 from .database import Connection
-from .journals import journals_fields
 from .journals import journals_table
 from .settings import settings_table
-from .submissions import submissions_fields
 from .submissions import submissions_table
-from .users import users_fields
-from .users import users_indexes
+from .update import get_version
 from .users import users_table
 
 
@@ -43,8 +40,7 @@ def merge_database(db_a: Connection, db_a_folder: str, db_b: Connection, db_b_fo
     :return: None
     """
 
-    db_a_version: str = db_a.execute(f"SELECT SVALUE FROM {settings_table} WHERE SETTING = 'FILESFOLDER'").fetchone()[0]
-    db_b_version: str = db_b.execute(f"SELECT SVALUE FROM {settings_table} WHERE SETTING = 'FILESFOLDER'").fetchone()[0]
+    db_a_version, db_b_version = get_version(db_a), get_version(db_b)
     if db_b_version is None or db_b_version is None:
         raise DatabaseError(f"Cannot read database versions: DB A: {db_a_version}, DB B: {db_b_version}")
     elif db_a_version != db_b_version:
@@ -60,28 +56,28 @@ def merge_database(db_a: Connection, db_a_folder: str, db_b: Connection, db_b_fo
     merge_folders(db_b_files_folder, db_a_files_folder)
 
     submission: tuple
-    for submission in db_b.execute(f"SELECT {','.join(submissions_fields)} FROM {submissions_table}"):
+    for submission in db_b.execute(f"SELECT * FROM {submissions_table}"):
         db_a.execute(
             f"""INSERT OR IGNORE INTO {submissions_table}
-            ({','.join(submissions_fields)}) VALUES ({','.join(['?'] * len(submissions_fields))})""",
+            (*) VALUES ({','.join(['?'] * len(submission))})""",
             submission
         )
     db_a.commit()
 
     journal: tuple
-    for journal in db_b.execute(f"SELECT {','.join(journals_fields)} FROM {journals_table}"):
+    for journal in db_b.execute(f"SELECT * FROM {journals_table}"):
         db_a.execute(
             f"""INSERT OR IGNORE INTO {journals_table}
-            ({','.join(journals_fields)}) VALUES ({','.join(['?'] * len(journals_fields))})""",
+            (*) VALUES ({','.join(['?'] * len(journal))})""",
             journal
         )
     db_a.commit()
 
     user_b: Tuple[str, ...]
-    for user_b in db_b.execute(f"SELECT {','.join(users_fields)} FROM {users_table}"):
-        user: str = user_b[users_indexes["USERNAME"]]
+    for user_b in db_b.execute(f"SELECT * FROM {users_table}"):
+        user: str = user_b[0]
         user_a: Optional[Tuple[str, ...]] = db_b.execute(
-            f"SELECT {','.join(users_fields)} FROM {users_table} WHERE USERNAME = ?", [user]).fetchone()
+            f"SELECT * FROM {users_table} WHERE USERNAME = ?", [user]).fetchone()
         user_new: List[str] = list(user_b)
 
         if user_a is not None:
@@ -90,7 +86,7 @@ def merge_database(db_a: Connection, db_a_folder: str, db_b: Connection, db_b_fo
                     user_new[i] = ",".join(filter(bool, set(field_a.split(",") + field_b.split(","))))
         db_a.execute(
             f"""INSERT OR REPLACE INTO {users_table}
-            ({','.join(users_fields)}) VALUES ({','.join(['?'] * len(users_fields))})""",
+            (*) VALUES ({','.join(['?'] * len(user_new))})""",
             user_new
         )
     db_a.commit()
