1681c87b421c7824b20f202a9506efe854067610
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-02-17 10:27:38 +0100

    database - add_to_list function to add items to a list field

--- falocalrepo_database/database.py
+++ falocalrepo_database/database.py
@@ -107,6 +107,15 @@ class FADatabaseTable:
         ][0]
         return self.column_id_
 
+    def add_to_list(self, key: Key, values: Dict[str, List[Value]]):
+        item: Optional[dict] = self[key]
+        if not item:
+            return False
+        item = {k: item[k] for k in values.keys()}
+        item_new: Dict[str, str] = {k: ",".join(sorted(set(item[k].split(",") + v))) for k, v in values.items()}
+        self.update(item_new, key) if item_new != item else None
+        return item_new != item
+
     def reload(self):
         self.__init__(self.database, self.table)
 
