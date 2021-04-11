5e663b884dc3574b6954e6f6d11c96660bda5713
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-04-11 16:11:15 +0200

    __init__ - export FADatabaseCursor

--- falocalrepo_database/__init__.py
+++ falocalrepo_database/__init__.py
@@ -1,5 +1,6 @@
 from .__version__ import __version__
 from .database import FADatabase
+from .database import FADatabaseCursor
 from .database import FADatabaseJournals
 from .database import FADatabaseSettings
 from .database import FADatabaseSubmissions
