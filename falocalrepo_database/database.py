e9a0f876a1d9ea9eb8ccb4fd08d30af135804f82
Matteo Campinoti <matteo.campinoti94@gmail.com>
2021-04-19 11:32:23 +0200

    database - get_submission_files returns paths instead of bytes objects

--- falocalrepo_database/database.py
+++ falocalrepo_database/database.py
@@ -9,6 +9,7 @@ from os.path import dirname
 from os.path import isdir
 from os.path import isfile
 from os.path import join
+from pathlib import Path
 from re import sub
 from shutil import copy
 from sqlite3 import Connection
@@ -299,13 +300,13 @@ class FADatabaseSettings(FADatabaseTable):
 
 
 class FADatabaseSubmissions(FADatabaseTable):
-    def get_submission_files(self, submission_id: int) -> tuple[Optional[bytes], Optional[bytes]]:
+    def get_submission_files(self, submission_id: int) -> tuple[Optional[str], Optional[str]]:
         if (entry := self[submission_id]) is None or (f := entry["FILESAVED"]) == 0:
             return None, None
         folder: str = join(self.database.files_folder, tiered_path(submission_id))
         file: str = join(folder, "submission" + f".{(ext := entry['FILEEXT'])}" * bool(ext)) if f >= 10 else None
         thumb: str = join(folder, "thumbnail.jpg") if f % 10 == 1 else None
-        return open(file, "rb").read() if file else None, open(thumb, "rb").read() if thumb else None
+        return file, thumb
 
     def save_submission_file(self, submission_id: int, file: Optional[bytes], name: str, ext: str,
                              guess_ext: bool = True) -> str:
