24fe3e77cf6ee21af161e6a87970455906d88eee
Matteo Campinoti <matteo.campinoti94@gmail.com>
2022-08-31 22:48:08 +0200

    tables - use base class and __annotations__ to get list of columns

--- falocalrepo_database/tables.py
+++ falocalrepo_database/tables.py
@@ -27,138 +27,73 @@ settings_table: str = "SETTINGS"
 history_table: str = "HISTORY"
 
 
-class UsersColumns:
-    USERNAME = Column("USERNAME", str, unique=True, key=True, check="length({name}) > 0", to_entry=clean_username)
-    FOLDERS = Column("FOLDERS", set)
-    ACTIVE = Column("ACTIVE", bool)
-    USERPAGE = Column("USERPAGE", str, to_entry=str.strip)
-
+class Columns:
     @classmethod
     def as_list(cls) -> list[Column]:
-        return [
-            cls.USERNAME,
-            cls.FOLDERS,
-            cls.ACTIVE,
-            cls.USERPAGE,
-        ]
+        return [getattr(cls, k) for k in cls.__annotations__.keys()]
 
 
-class SubmissionsColumns:
-    ID = Column("ID", int, unique=True, key=True, check="{name} > 0")
-    AUTHOR = Column("AUTHOR", str, check="length({name}) > 0")
-    TITLE = Column("TITLE", str)
-    DATE = Column("DATE", datetime)
-    DESCRIPTION = Column("DESCRIPTION", str)
-    FOOTER = Column("FOOTER", str)
-    TAGS = Column("TAGS", list)
-    CATEGORY = Column("CATEGORY", str)
-    SPECIES = Column("SPECIES", str)
-    GENDER = Column("GENDER", str)
-    RATING = Column("RATING", str)
-    TYPE = Column("TYPE", str, to_entry=str.lower, check="{name} in ('image', 'music', 'text', 'flash')")
-    FILEURL = Column("FILEURL", list[str], from_entry=lambda v: parse_list(v))
-    FILEEXT = Column("FILEEXT", list[str], from_entry=lambda v: parse_list(v))
-    FILESAVED = Column("FILESAVED", int, check="{name} in (0, 1, 2, 3, 4, 5, 6, 7)")
-    FAVORITE = Column("FAVORITE", set)
-    MENTIONS = Column("MENTIONS", set)
-    FOLDER = Column("FOLDER", str, to_entry=str.lower, check="{name} in ('gallery', 'scraps')")
-    USERUPDATE = Column("USERUPDATE", bool)
-
-    @classmethod
-    def as_list(cls) -> list[Column]:
-        return [
-            cls.ID,
-            cls.AUTHOR,
-            cls.TITLE,
-            cls.DATE,
-            cls.DESCRIPTION,
-            cls.FOOTER,
-            cls.TAGS,
-            cls.CATEGORY,
-            cls.SPECIES,
-            cls.GENDER,
-            cls.RATING,
-            cls.TYPE,
-            cls.FILEURL,
-            cls.FILEEXT,
-            cls.FILESAVED,
-            cls.FAVORITE,
-            cls.MENTIONS,
-            cls.FOLDER,
-            cls.USERUPDATE,
-        ]
+class UsersColumns(Columns):
+    USERNAME: Column = Column("USERNAME", str, unique=True, key=True, check="length({name}) > 0",
+                              to_entry=clean_username)
+    FOLDERS: Column = Column("FOLDERS", set)
+    ACTIVE: Column = Column("ACTIVE", bool)
+    USERPAGE: Column = Column("USERPAGE", str, to_entry=str.strip)
 
 
-class JournalsColumns:
-    ID = Column("ID", int, unique=True, key=True, check="{name} > 0")
-    AUTHOR = Column("AUTHOR", str, check="length({name}) > 0")
-    TITLE = Column("TITLE", str)
-    DATE = Column("DATE", datetime)
-    CONTENT = Column("CONTENT", str)
-    HEADER = Column("HEADER", str)
-    FOOTER = Column("FOOTER", str)
-    MENTIONS = Column("MENTIONS", set)
-    USERUPDATE = Column("USERUPDATE", bool)
-
-    @classmethod
-    def as_list(cls) -> list[Column]:
-        return [
-            cls.ID,
-            cls.AUTHOR,
-            cls.TITLE,
-            cls.DATE,
-            cls.CONTENT,
-            cls.HEADER,
-            cls.FOOTER,
-            cls.MENTIONS,
-            cls.USERUPDATE,
-        ]
+class SubmissionsColumns(Columns):
+    ID: Column = Column("ID", int, unique=True, key=True, check="{name} > 0")
+    AUTHOR: Column = Column("AUTHOR", str, check="length({name}) > 0")
+    TITLE: Column = Column("TITLE", str)
+    DATE: Column = Column("DATE", datetime)
+    DESCRIPTION: Column = Column("DESCRIPTION", str)
+    FOOTER: Column = Column("FOOTER", str)
+    TAGS: Column = Column("TAGS", list)
+    CATEGORY: Column = Column("CATEGORY", str)
+    SPECIES: Column = Column("SPECIES", str)
+    GENDER: Column = Column("GENDER", str)
+    RATING: Column = Column("RATING", str)
+    TYPE: Column = Column("TYPE", str, to_entry=str.lower, check="{name} in ('image', 'music', 'text', 'flash')")
+    FILEURL: Column = Column("FILEURL", list[str], from_entry=lambda v: parse_list(v))
+    FILEEXT: Column = Column("FILEEXT", list[str], from_entry=lambda v: parse_list(v))
+    FILESAVED: Column = Column("FILESAVED", int, check="{name} in (0, 1, 2, 3, 4, 5, 6, 7)")
+    FAVORITE: Column = Column("FAVORITE", set)
+    MENTIONS: Column = Column("MENTIONS", set)
+    FOLDER: Column = Column("FOLDER", str, to_entry=str.lower, check="{name} in ('gallery', 'scraps')")
+    USERUPDATE: Column = Column("USERUPDATE", bool)
 
 
-class CommentsColumns:
-    ID = Column("ID", int, key=True, check="{name} > 0")
-    PARENT_TABLE = Column("PARENT_TABLE", str, key=True,
-                          check=f"{'{name}'} in ('{submissions_table}', '{journals_table}')")
-    PARENT_ID = Column("PARENT_ID", int, key=True, check="{name} > 0")
-    REPLY_TO = Column("REPLY_TO", int, not_null=False, check="{name} == null or {name} > 0")
-    AUTHOR = Column("AUTHOR", str, check="length({name}) > 0")
-    DATE = Column("DATE", datetime, to_entry=lambda v: v.strftime("%Y-%m-%dT%H:%M:%S"),
-                  from_entry=lambda v: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S"))
-    TEXT = Column("TEXT", str)
-
-    @classmethod
-    def as_list(cls) -> list[Column]:
-        return [
-            cls.ID,
-            cls.PARENT_TABLE,
-            cls.PARENT_ID,
-            cls.REPLY_TO,
-            cls.AUTHOR,
-            cls.DATE,
-            cls.TEXT,
-        ]
+class JournalsColumns(Columns):
+    ID: Column = Column("ID", int, unique=True, key=True, check="{name} > 0")
+    AUTHOR: Column = Column("AUTHOR", str, check="length({name}) > 0")
+    TITLE: Column = Column("TITLE", str)
+    DATE: Column = Column("DATE", datetime)
+    CONTENT: Column = Column("CONTENT", str)
+    HEADER: Column = Column("HEADER", str)
+    FOOTER: Column = Column("FOOTER", str)
+    MENTIONS: Column = Column("MENTIONS", set)
+    USERUPDATE: Column = Column("USERUPDATE", bool)
 
 
-class SettingsColumns:
-    SETTING = Column("SETTING", str, unique=True, key=True, check="length({name}) > 0")
-    SVALUE = Column("SVALUE", str, not_null=False, check="{name} == null or length({name}) > 0")
+class CommentsColumns(Columns):
+    ID: Column = Column("ID", int, key=True, check="{name} > 0")
+    PARENT_TABLE: Column = Column("PARENT_TABLE", str, key=True,
+                                  check=f"{'{name}'} in ('{submissions_table}', '{journals_table}')")
+    PARENT_ID: Column = Column("PARENT_ID", int, key=True, check="{name} > 0")
+    REPLY_TO: Column = Column("REPLY_TO", int, not_null=False, check="{name} == null or {name} > 0")
+    AUTHOR: Column = Column("AUTHOR", str, check="length({name}) > 0")
+    DATE: Column = Column("DATE", datetime, to_entry=lambda v: v.strftime("%Y-%m-%dT%H:%M:%S"),
+                          from_entry=lambda v: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S"))
+    TEXT: Column = Column("TEXT", str)
 
-    @classmethod
-    def as_list(cls) -> list[Column]:
-        return [
-            cls.SETTING,
-            cls.SVALUE,
-        ]
+
+class SettingsColumns(Columns):
+    SETTING: Column = Column("SETTING", str, unique=True, key=True, check="length({name}) > 0")
+    SVALUE: Column = Column("SVALUE", str, not_null=False, check="{name} == null or length({name}) > 0")
 
 
-class HistoryColumns:
-    TIME = Column("TIME", datetime, unique=True, key=True, to_entry=lambda v: v.strftime("%Y-%m-%dT%H:%M:%S.%f"),
-                  from_entry=lambda v: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
-    EVENT = Column("EVENT", str)
-
-    @classmethod
-    def as_list(cls) -> list[Column]:
-        return [
-            cls.TIME,
-            cls.EVENT,
-        ]
+class HistoryColumns(Columns):
+    TIME: Column = Column("TIME", datetime, unique=True, key=True,
+                          to_entry=lambda v: v.strftime("%Y-%m-%dT%H:%M:%S.%f"),
+                          from_entry=lambda v: datetime.strptime(v, "%Y-%m-%dT%H:%M:%S.%f"))
+    EVENT: Column = Column("EVENT", str)
